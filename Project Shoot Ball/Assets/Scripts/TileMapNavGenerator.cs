2388248dc9841f7d0e2c0c6b0f7f4dd4185c69e0
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2015-08-17 16:16:47 +0100

    Imported polynav asset and set up test scene

---
+++ Project Shoot Ball/Assets/Scripts/TileMapNavGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMapNavGenerator : MonoBehaviour {
+
+    public float extraOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+        foreach(TileEditor.Tile tile in FindObjectsOfType<TileEditor.Tile>())
+        {
+            if(tile.GetComponent<Collider2D>() != null)
+            {
+                PolyNavObstacle ob = tile.gameObject.AddComponent<PolyNavObstacle>();
+                ob.extraOffset = extraOffset;
+            }
+        }
+
+	}
+}
