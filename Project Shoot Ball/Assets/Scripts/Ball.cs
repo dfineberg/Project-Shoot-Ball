8a4378673fe94114c20e717376214d9507dc2fc1
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2015-08-27 15:06:18 +0100

    Revert "Revert "Players can now throw and catch the ball""
    This reverts commit abda453e1b61faef92d1753b58dca189f3583ff2.

---
+++ Project Shoot Ball/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    PlayerController carrier;
+    Rigidbody2D rigidbody;
+    Collider2D collider;
+    static Ball instance;
+
+
+    void Start()
+    {
+        instance = this;
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    public static PlayerController GetCarrier()
+    {
+        return instance.carrier;
+    }
+}
