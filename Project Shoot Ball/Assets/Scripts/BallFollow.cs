a1022297252c76a712685d91adcf45356b069670
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2015-09-05 13:50:31 +0100

    Enemies now follow the ball

---
+++ Project Shoot Ball/Assets/Scripts/BallFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PolyNavAgent))]
+public class BallFollow : MonoBehaviour {
+
+    PolyNavAgent agent;
+    GameObject ball;
+    public static bool chasing = true;
+
+    void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball");
+        agent = GetComponent<PolyNavAgent>();
+    }
+
+    void Update()
+    {
+        if (chasing)
+        {
+            agent.SetDestination(ball.transform.position);
+        }
+    }
+}
