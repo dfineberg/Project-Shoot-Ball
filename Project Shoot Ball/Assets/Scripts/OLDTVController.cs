f1470d4c8295e7dcb01f2324c3ab56c878286859
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2015-10-03 15:18:16 +0100

    Added screen interference on explosions

---
+++ Project Shoot Ball/Assets/Scripts/OLDTVController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class TVEvent
+{
+    public float aberration;
+    public float noise;
+    public float staticMagnitude;
+    public float time;
+}
+
+public class OLDTVController : MonoBehaviour {
+
+    public TVEvent explosionEvent;
+    public float staticScrollSpeed;
+
+    Animator animator;
+    int explosionHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        explosionHash = Animator.StringToHash("Explosion");
+
+        Grenade.e_explode += Explode;
+    }
+
+    void OnDisable()
+    {
+        Grenade.e_explode -= Explode;
+    }
+
+    void Explode()
+    {
+        animator.SetTrigger(explosionHash);
+    }
+}
