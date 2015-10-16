3fa6b00673f5b1775d7a75f95b35a6a7eec076d9
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2015-10-16 17:19:33 +0100

    Added bounce prediction to aim sight and fixed erratic bounces

---
+++ Project Shoot Ball/Assets/Scripts/PhysicsReflector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsReflector : MonoBehaviour {
+
+    [Range(0f, 1f)]
+    public float bounce = 0.8f;
+
+    new Rigidbody2D rigidbody;
+    Vector2 velocityCache;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        velocityCache = rigidbody.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector2 reflectedVelocity = Vector2.Reflect(velocityCache, col.contacts[0].normal);
+        reflectedVelocity *= bounce;
+        rigidbody.velocity = reflectedVelocity;
+    }
+}
